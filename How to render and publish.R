704cd3ff4ea6daa44bd3e958d181e8eb1be6f0ca
Markus Bernhardt-Römermann <su28yek@ads.uni-jena.de>
2024-06-18 15:15:15 +0200

    corrected order in legend

--- How to render and publish.R
+++ How to render and publish.R
@@ -1,6 +1,8 @@
 # to publish the webpage, please run the following lines in the Terminal
-
 quarto render
 git add docs
 git commit -m "Publish site to docs/"
 git push
+
+git lfs migrate import --include="*.shp"
+git lfs migrate import --include="*.dbf"
